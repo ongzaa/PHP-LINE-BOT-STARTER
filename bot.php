00ad99a8e7453524c09475546671366b4e92c427
ongzaa <31213832+ongzaa@users.noreply.github.com>
2017-08-22 14:33:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			
-			
+			$text = $text." Reply by Piak";
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
