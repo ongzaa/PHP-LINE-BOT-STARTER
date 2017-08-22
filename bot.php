c0ccc14d6b44dc039702b2040a145f076e3dcd3e
ongzaa <31213832+ongzaa@users.noreply.github.com>
2017-08-22 13:54:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,47 @@
-<?PHP
-echo "I'm BOT for ECHOIDEA";
-?>
+<?php
+$access_token = 'Tw3Q8ZD5sM0v64J4d4EA3xIPhHuomz+dZu0fFIAkBqVfwmy/hmF7bx2EPmQGpRrPdzy1+Sjfpi8GLpXV2kIixMC0Pl4yIKq2tqGVSFLns90PL76EB2+PO0FDvN9CNECtu6exP6jOjy9PgiF92TmmngdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
