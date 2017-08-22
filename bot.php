354e92b3e588b4e475f379928499e6907281f58b
ongzaa <31213832+ongzaa@users.noreply.github.com>
2017-08-22 16:16:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,10 +30,10 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' 		=> 'location',
-					'title'		=> 'My Location'
+					'title'		=> 'My Location',
 					'address'	=> 'Bangkok, Thailand',
 					'latitude'	=> '35.65910807942215',
-					'longitude' => '139.70372892916203'
+					'longitude' 	=> '139.70372892916203'
 				];
 
 			}else{
